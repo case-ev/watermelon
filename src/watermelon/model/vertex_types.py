7bb6ad2ae8c9ac23e713568e1aa7b26a0e4a0346
Cristobal Allendes <callendes.molina@gmail.com>
2023-01-27 14:48:46 -0300

    Style changes

--- src/watermelon/model/vertex_types.py
+++ src/watermelon/model/vertex_types.py
@@ -4,8 +4,10 @@ from watermelon.model.vertex_actions import NULL_ACTION
 
 class VertexType(abc.ABC):
     """Base interface for a node type."""
+
     @abc.abstractstaticmethod
-    def _char(self): pass
+    def _char(self):
+        pass
 
     def __repr__(self):
         return self.__class__.__name__
@@ -16,8 +18,9 @@ class VertexType(abc.ABC):
 
 class EMPTY_VERTEX_TYPE(VertexType):
     """Empty node."""
+
     ACTIONS = [NULL_ACTION]
 
     @staticmethod
     def _char(self):
-        return "\u03b8"     # theta
+        return "\u03b8"  # theta
