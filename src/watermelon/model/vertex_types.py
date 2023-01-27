94061cc3fd88b9284a6b8a0985e5beaceb9ef9c3
Cristobal Allendes <callendes.molina@gmail.com>
2023-01-27 14:34:15 -0300

    Added methods to debug vertex types

--- src/watermelon/model/vertex_types.py
+++ src/watermelon/model/vertex_types.py
@@ -1,11 +1,23 @@
+import abc
 from watermelon.model.vertex_actions import NULL_ACTION
 
 
-class VertexType:
+class VertexType(abc.ABC):
     """Base interface for a node type."""
-    pass
+    @abc.abstractstaticmethod
+    def _char(self): pass
+
+    def __repr__(self):
+        return self.__class__.__name__
+
+    def __str__(self):
+        return self._char()
 
 
 class EMPTY_VERTEX_TYPE(VertexType):
     """Empty node."""
     ACTIONS = [NULL_ACTION]
+
+    @staticmethod
+    def _char(self):
+        return "\u03b1"
