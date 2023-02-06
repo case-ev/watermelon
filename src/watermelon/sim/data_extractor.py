19ed1526e4984dd9c5f38ea08b94bd795a9374d1
Cristobal Allendes <callendes.molina@gmail.com>
2023-02-06 18:02:27 -0300

    Reformatted the way data extractors are handled

--- src/watermelon/sim/data_extractor.py
+++ src/watermelon/sim/data_extractor.py
@@ -2,15 +2,18 @@ import pandas as pd
 
 
 class DataFrameExtractor:
-    def __init__(self, *args, **kwargs):
-        self.data = pd.DataFrame(*args, **kwargs)
+    def __init__(self, agents, initial_state):
+        self.data = pd.DataFrame(self.parse_data(agents, initial_state, 0))
 
     def append(self, simulation_state):
-        data = {
-            **{"time": [simulation_state.time]},
-            **{
-                a: action
-                for a, action in zip(simulation_state.agents, simulation_state.state)
-            },
-        }
+        data = self.parse_data(
+            simulation_state.agents, simulation_state.state, simulation_state.time
+        )
         self.data = pd.concat([self.data, pd.DataFrame(data)], ignore_index=True)
+
+    @staticmethod
+    def parse_data(agents, state, time=0):
+        return {
+            **{"time": [time]},
+            **{a: action for a, action in zip(agents, state)},
+        }
--- src/watermelon/sim/simulator.py
+++ src/watermelon/sim/simulator.py
@@ -12,12 +12,7 @@ class Simulator:
         self.state = np.array([a.actions[0] for a in agents])
 
     def start(self, extractor_cls=DataFrameExtractor):
-        self.data_extractor = extractor_cls(
-            data={
-                **{"time": [0]},
-                **{a: action for a, action in zip(self.agents, self.state)},
-            }
-        )
+        self.data_extractor = extractor_cls(self.agents, self.state)
 
     def update(self):
         self.time += self.delta
