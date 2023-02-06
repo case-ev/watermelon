a96690a73145650c350245944e090b5aa2efa481
Cristobal Allendes <callendes.molina@gmail.com>
2023-02-06 16:52:07 -0300

    Modernized the example running script

--- examples/__main__.py
+++ examples/__main__.py
@@ -1,6 +1,7 @@
 import sys
 import pathlib
 import os
+import importlib
 
 # Change the import path
 src_path = pathlib.Path.joinpath(pathlib.Path(__file__).parent.parent, "src")
@@ -11,13 +12,12 @@ try:
 except Exception:
     pass
 
-from watermelon_common.logger import setup_logger
+from watermelon_common.logger import setup_logger, LOGGER
 import argparse
-import examples
 
 
 # Set up argument parsing
-desc_str = """run the examples in watermelon"""
+desc_str = """run the examples"""
 
 PARSER = argparse.ArgumentParser(prog="examples", description=desc_str)
 PARSER.add_argument(
@@ -53,8 +53,21 @@ PARSER.add_argument(
 cmd_args = PARSER.parse_args()
 setup_logger(cmd_args.quiet, cmd_args.debug, cmd_args.verbose, cmd_args.log)
 
-# Run the examples
-for arg in cmd_args.args:
-    print(f"\nExecuting example '\x1b[32;20m{arg}\x1b[0m'\n")
-    with open(os.path.join("examples", f"{arg}.py"), "r") as f:
-        exec(f.read())
+# Get the attributes
+name = cmd_args.args[0]
+ex_args = []
+ex_kwargs = {}
+LOGGER.info("Parsing args and kwargs")
+for arg in map(lambda x: x.split("="), cmd_args.args[1:]):
+    if len(arg) == 2:
+        key, val = arg
+        ex_kwargs[key] = val
+    else:
+        ex_args.extend(arg)
+
+# Run the example
+print(f"\nExecuting example '\x1b[32;20m{name}\x1b[0m' with args {ex_args} and kwargs {ex_kwargs}\n")
+LOGGER.info(f"Importing example {name}")
+mod = importlib.import_module(f"examples.{name}")
+LOGGER.info(f"Running main function")
+mod.main(*ex_args, **ex_kwargs)
