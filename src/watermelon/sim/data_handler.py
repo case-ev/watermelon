60ae0fbb01a96ffaa21583130474d82f562cd838
Cristobal Allendes <callendes.molina@gmail.com>
2023-02-06 17:50:01 -0300

    Added the append method to the DataExtractor class

--- src/watermelon/sim/data_handler.py
+++ src/watermelon/sim/data_handler.py
@@ -4,3 +4,13 @@ import pandas as pd
 class DataExtractor:
     def __init__(self, *args, **kwargs):
         self.data = pd.DataFrame(*args, **kwargs)
+
+    def append(self, simulation_state):
+        data = {
+            **{"time": [simulation_state.time]},
+            **{
+                a: action
+                for a, action in zip(simulation_state.agents, simulation_state.state)
+            },
+        }
+        self.data = pd.concat([self.data, pd.DataFrame(data)], ignore_index=True)
