0005e613c0c9664c6cf448fa6cc6f764a96fdb57
Cristobal Allendes <callendes.molina@gmail.com>
2023-02-06 17:38:13 -0300

    Customized the vertex actions

--- src/watermelon/model/__init__.py
+++ src/watermelon/model/__init__.py
@@ -1,4 +1,4 @@
 from .agent import Agent
 from .graph import draw_graph, Edge, Graph, Vertex
-from .vertex_actions import VertexAction, NULL_ACTION
+from .vertex_actions import Decision, VertexAction, NULL_ACTION
 from .vertex_types import VertexType, EMPTY_VERTEX_TYPE
--- src/watermelon/model/vertex_actions.py
+++ src/watermelon/model/vertex_actions.py
@@ -1,6 +1,29 @@
+class Decision:
+    def __init__(self, vertex, action):
+        self.vertex = vertex
+        self.action = action
+
+    def __repr__(self):
+        return f"Decision({repr(self.vertex)}, {repr(self.action)})"
+
+    def __str__(self):
+        return f"({str(self.vertex)}, {str(self.action)})"
+
+
 class VertexAction:
-    pass
+    @classmethod
+    def __repr__(cls):
+        return cls.__class__.__name__
+
+    @classmethod
+    def __str__(cls):
+        return cls._char()
 
 
-class NULL_ACTION(VertexAction):
-    pass
+class __NullAction(VertexAction):
+    @staticmethod
+    def _char():
+        return "\u03d5"  # phi
+
+
+NULL_ACTION = __NullAction()
