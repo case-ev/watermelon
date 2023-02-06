73ec470541222790d1bc73f949476b04983c85f3
Cristobal Allendes <callendes.molina@gmail.com>
2023-02-06 15:24:00 -0300

    Added an import to the agent

--- src/watermelon/model/__init__.py
+++ src/watermelon/model/__init__.py
@@ -1,3 +1,4 @@
+from .agent import Agent
 from .graph import draw_graph, Edge, Graph, Vertex
 from .vertex_actions import VertexAction, NULL_ACTION
 from .vertex_types import VertexType, EMPTY_VERTEX_TYPE
