f6ca4f9bd3837d864a1cc5cfe517e954dacc220b
Cristobal Allendes <callendes.molina@gmail.com>
2023-02-09 17:35:48 -0300

    Modernized the notation in the hello_world example

--- examples/hello_world.py
+++ examples/hello_world.py
@@ -7,17 +7,17 @@ Example to show the creation of a basic graph.
 import matplotlib.pyplot as plt
 
 from watermelon_common.logger import LOGGER
-from watermelon.model import Edge, Graph, Vertex, draw_graph
+import watermelon as wm
 
 
 def main():
     """Entry point for the example"""
 
     LOGGER.info("Creating vertices")
-    verts = [Vertex(i) for i in range(5)]
+    verts = [wm.Vertex(i) for i in range(5)]
 
     LOGGER.info("Initializing graph")
-    graph = Graph()
+    graph = wm.Graph()
     graph.add_vertices(verts)
     print("Graph without edges")
     print(graph)
@@ -25,14 +25,14 @@ def main():
     LOGGER.info("Creating edges")
     graph.add_edges(
         [
-            Edge(Vertex(0), Vertex(1), 10),
-            Edge(Vertex(1), Vertex(0), 4),
-            Edge(Vertex(0), Vertex(2), 3),
-            Edge(Vertex(2), Vertex(1), 2),
-            Edge(Vertex(2), Vertex(4), 6),
-            Edge(Vertex(4), Vertex(2), 2),
-            Edge(Vertex(4), Vertex(3), 7),
-            Edge(Vertex(3), Vertex(1), 2),
+            wm.Edge(wm.Vertex(0), wm.Vertex(1), 10),
+            wm.Edge(wm.Vertex(1), wm.Vertex(0), 4),
+            wm.Edge(wm.Vertex(0), wm.Vertex(2), 3),
+            wm.Edge(wm.Vertex(2), wm.Vertex(1), 2),
+            wm.Edge(wm.Vertex(2), wm.Vertex(4), 6),
+            wm.Edge(wm.Vertex(4), wm.Vertex(2), 2),
+            wm.Edge(wm.Vertex(4), wm.Vertex(3), 7),
+            wm.Edge(wm.Vertex(3), wm.Vertex(1), 2),
         ]
     )
     print("\nGraph with edges")
@@ -40,7 +40,7 @@ def main():
 
     LOGGER.info("Drawing graph")
     fig, ax = plt.subplots()
-    draw_graph(graph, axis=ax)
+    wm.draw_graph(graph, axis=ax)
 
     fig.suptitle("Hello World!")
     ax.set_xlabel("X axis")
