07ba94fcd9955b987870a94e6df14b2609b5dfc3
Cristobal Allendes <callendes.molina@gmail.com>
2023-02-09 12:27:38 -0300

    Style changes

--- examples/__main__.py
+++ examples/__main__.py
@@ -1,7 +1,14 @@
+"""
+examples.__main__
+-----------------
+Act as an entry point for the examples module.
+"""
+
 import sys
 import pathlib
 import os
 import importlib
+import argparse
 
 # Change the import path
 src_path = pathlib.Path.joinpath(pathlib.Path(__file__).parent.parent, "src")
@@ -9,44 +16,43 @@ os.environ["PYTHONPATH"] = str(src_path)
 sys.path.append(str(src_path))
 
 from watermelon_common.logger import setup_logger, LOGGER
-import argparse
 
 
 # Set up argument parsing
-desc_str = """run the examples"""
+DESC_STR = """run the examples"""
 
-PARSER = argparse.ArgumentParser(prog="examples", description=desc_str)
-PARSER.add_argument(
+parser = argparse.ArgumentParser(prog="examples", description=DESC_STR)
+parser.add_argument(
     "args",
     nargs="+",
     help="program and its arguments",
 )
-PARSER.add_argument(
+parser.add_argument(
     "-d",
     "--debug",
     action="store_true",
     help="run in debug mode",
 )
-PARSER.add_argument(
+parser.add_argument(
     "--log",
     action="store",
     default=os.path.join(src_path.parent, "logs"),
     help="generate log files for the current run in the given directory",
 )
-PARSER.add_argument(
+parser.add_argument(
     "-q",
     "--quiet",
     action="store_true",
     help="disable logging",
 )
-PARSER.add_argument(
+parser.add_argument(
     "-v",
     "--verbose",
     action="store_true",
     help="run in verbose mode",
 )
 
-cmd_args = PARSER.parse_args()
+cmd_args = parser.parse_args()
 try:
     setup_logger(cmd_args.quiet, cmd_args.debug, cmd_args.verbose, cmd_args.log)
 except FileNotFoundError:
@@ -69,7 +75,7 @@ for arg in map(lambda x: x.split("="), cmd_args.args[1:]):
 print(
     f"\nExecuting example '\x1b[32;20m{name}\x1b[0m' with args {ex_args} and kwargs {ex_kwargs}\n"
 )
-LOGGER.info(f"Importing example {name}")
+LOGGER.info("Importing example %s", name)
 mod = importlib.import_module(f"examples.{name}")
-LOGGER.info(f"Running main function")
+LOGGER.info("Running main function")
 mod.main(*ex_args, **ex_kwargs)
