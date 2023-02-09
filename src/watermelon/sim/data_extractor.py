234d8e5920b4452486b0a2fa7c4db5d43f6e96ea
Cristobal Allendes <callendes.molina@gmail.com>
2023-02-09 11:19:52 -0300

    Added more documentation

--- src/watermelon/sim/data_extractor.py
+++ src/watermelon/sim/data_extractor.py
@@ -1,11 +1,26 @@
+"""
+watermelon.sim.data_extractor
+-----------------------------
+Code that defines the class that extracts data from the simulation
+and transforms it into the correct format.
+"""
+
 import pandas as pd
 
 
 class DataFrameExtractor:
+    """Object that extracts data into a pandas.DataFrame object.
+
+    In the dataframe that is created, there are columns which indicate
+    each agent involved in the graph. Each row has a timestamp and
+    indicates the state of each agent at every instant.
+    """
+
     def __init__(self, agents, initial_state):
         self.data = pd.DataFrame(self.parse_data(agents, initial_state, 0))
 
     def append(self, simulation_state):
+        """Get the data from the simulator and append it"""
         data = self.parse_data(
             simulation_state.agents, simulation_state.state, simulation_state.time
         )
@@ -13,7 +28,8 @@ class DataFrameExtractor:
 
     @staticmethod
     def parse_data(agents, state, time=0):
+        """Parse some input data according to the required format"""
         return {
             **{"time": [time]},
-            **{a: action for a, action in zip(agents, state)},
+            **dict(zip(agents, state)),
         }
--- src/watermelon/sim/simulator.py
+++ src/watermelon/sim/simulator.py
@@ -1,8 +1,19 @@
-from watermelon.sim.data_extractor import DataFrameExtractor
+"""
+watermelon.sim.simulator
+------------------------
+Main functionality of the simulator, which takes a graph and a collection
+of agents and runs the simulation given their decisions.
+"""
+
 import numpy as np
+
+from watermelon_common.logger import LOGGER
+from watermelon.sim.data_extractor import DataFrameExtractor
 
 
 class Simulator:
+    """Object that simulates the graph"""
+
     def __init__(self, graph, agents, *, delta=1e-3):
         self.graph = graph
         self.agents = agents
@@ -10,13 +21,22 @@ class Simulator:
         self.delta = delta
         self.time = 0.0
         self.state = np.array([a.actions[0] for a in agents])
+        self.data_extractor = None
 
     def start(self, extractor_cls=DataFrameExtractor):
+        """Start the simulation. It must be ran before you start updating"""
         self.data_extractor = extractor_cls(self.agents, self.state)
 
     def update(self):
+        """Update the simulation. Should be run at every timestep"""
         self.time += self.delta
 
         # State update code ...
 
-        self.data_extractor.append(self)
+        try:
+            self.data_extractor.append(self)
+        except Exception as e:
+            LOGGER.error(
+                "Failed to append data to the extractor. Did you forget to start the simulation?"
+            )
+            raise e
