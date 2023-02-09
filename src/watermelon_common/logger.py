8496b3e627a176aab3585dab00994d87e6d44764
Cristobal Allendes <callendes.molina@gmail.com>
2023-02-09 12:01:25 -0300

    Modernized the logger

--- src/watermelon_common/logger.py
+++ src/watermelon_common/logger.py
@@ -1,6 +1,15 @@
+"""
+watermelon_common.logger
+------------------------
+Creation and setup for the logger used by watermelon and its related
+packages.
+"""
+
 import logging
 import os
 from datetime import datetime
+
+from watermelon_utils.stdout import AsciiColors
 
 
 LOGGER = logging.Logger("watermelon")
@@ -14,36 +23,50 @@ log_fmt = logging.Formatter(
 
 # Formatter for the console
 class CustomFormatter(logging.Formatter):
-    GREY = "\x1b[38;20m"
-    CYAN = "\x1b[36;20m"
-    YELLOW = "\x1b[33;20m"
-    GREEN = "\x1b[32;20m"
-    RED = "\x1b[31;20m"
-    BOLD_RED = "\x1b[31;1m"
-    RESET = "\x1b[0m"
+    """Custom formatter for the watermelon logger"""
 
-    FMT = "\x1b[45m[%(asctime)s](%(filename)s:%(lineno)d)\x1b[49m\x1b[0m {}%(levelname)s\x1b[0m :: %(message)s"
+    FMT = f"{AsciiColors.BG_MAGENTA}[%(asctime)s](%(filename)s:%(lineno)d){AsciiColors.BG_RESET} \
+{{}}%(levelname)s{AsciiColors.RESET} :: %(message)s"
     DATE_FMT = "%Y-%m-%d|%H:%M:%S"
-
     COLORS = {
-        logging.DEBUG: CYAN,
-        logging.INFO: GREEN,
-        logging.WARNING: YELLOW,
-        logging.ERROR: RED,
-        logging.CRITICAL: BOLD_RED,
+        logging.DEBUG: AsciiColors.CYAN,
+        logging.INFO: AsciiColors.GREEN,
+        logging.WARNING: AsciiColors.YELLOW,
+        logging.ERROR: AsciiColors.RED,
+        logging.CRITICAL: AsciiColors.BOLD_RED,
     }
 
     def format(self, record):
-        log_fmt = self.FMT.format(self.COLORS.get(record.levelno))
+        fmt = self.FMT.format(self.COLORS.get(record.levelno))
         formatter = logging.Formatter(
-            fmt=log_fmt,
+            fmt=fmt,
             datefmt=self.DATE_FMT,
         )
         return formatter.format(record)
 
 
 def setup_logger(quiet=False, debug=False, verbose=False, log_dir=False):
-    global LOGGER
+    """Setup the logger.
+
+    This must be run at the beginning of every program that uses
+    watermelon or its related packages.
+
+    Parameters
+    ----------
+    quiet : bool, optional
+        Whether to silence all logs, by default False
+    debug : bool, optional
+        Run in debug mode, which shows all log levels and adds extra
+        functionality that might be useful when debugging a watermelon
+        program, by default False
+    verbose : bool, optional
+        Run in verbose mode, which shows all logs at INFO level, by
+        default False
+    log_dir : bool, optional
+        Directory to save all logs, by default False. If it it False,
+        the program doesn't store the logs; if it is some value, it
+        assumes
+    """
 
     if not quiet:
         # Stream logger
@@ -57,13 +80,19 @@ def setup_logger(quiet=False, debug=False, verbose=False, log_dir=False):
             str_hdl.setLevel(logging.WARNING)
         LOGGER.addHandler(str_hdl)
 
+    # File logger
     if log_dir:
-        # File logger
-        now = datetime.now()
-        file_name = f"{now.year}{now.month:02}{now.day:02}_{now.hour:02}{now.minute:02}{now.second:02}"
-        file_path = os.path.join(log_dir, f"{file_name}.txt")
+        if "." in log_dir:
+            # Assume log_dir is a file
+            file_path = log_dir
+        else:
+            # Assume log_dir is a directory
+            now = datetime.now()
+            file_name = f"{now.year}{now.month:02}{now.day:02}_{now.hour:02}\
+{now.minute:02}{now.second:02}"
+            file_path = os.path.join(log_dir, f"{file_name}.txt")
         file_hdl = logging.FileHandler(file_path, encoding="utf-8")
         file_hdl.setFormatter(log_fmt)
         file_hdl.setLevel(logging.DEBUG)
         LOGGER.addHandler(file_hdl)
-        LOGGER.debug(f"Log file: {os.path.abspath(file_path)}")
+        LOGGER.debug("Log file: %s", os.path.abspath(file_path))
