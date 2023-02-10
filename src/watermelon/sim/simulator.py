d70e1948ee60b9be4ade1b0daf5d274d2124f1fb
Cristobal Allendes <callendes.molina@gmail.com>
2023-02-10 15:17:55 -0300

    Made a very rough implementation of the update algorithm

--- src/watermelon/sim/simulator.py
+++ src/watermelon/sim/simulator.py
@@ -14,13 +14,14 @@ from watermelon.sim.data_extractor import DataFrameExtractor
 class Simulator:
     """Object that simulates the graph"""
 
-    def __init__(self, graph, agents, *, delta=1e-3):
+    def __init__(self, graph, agents, *, delta=1e-3, battery_eff=0.75):
         self.graph = graph
         self.agents = agents
         self.delta = delta
         self.time = 0.0
         self.state = np.array([a.actions[0] for a in agents])
         self.data_extractor = None
+        self.battery_eff = 0.75
 
     def start(self, extractor_cls=DataFrameExtractor):
         """Start the simulation. It must be ran before you start updating"""
@@ -33,32 +34,52 @@ class Simulator:
         # State update code
         for agent in self.agents:
             agent.state.action_time += self.delta
-            decision = agent.actions[agent.state.current_action]
-            vertex, action = decision.tuple()
 
-            if agent.state.is_travelling[0]:
-                # It is travelling to a vertex
-                _, origin, target = agent.state.is_travelling
-                edge = self.graph.get_edge(origin, target)
-                travel_time = edge.time
-                if agent.state.action_time > travel_time:
-                    agent.state.is_travelling = (False, None, None)
-                    agent.state.just_arrived = True
-            else:
-                # It is doing some action
-                # if agent.state.just_arrived:
-                #     if vertex.capacity >=
-                time, energy = action.act(agent, vertex)
+            if not agent.state.is_done:
+                decision = agent.actions[agent.state.current_action]
+                vertex, action = decision.tuple()
 
-            # Go to the next action if appropiate
-            if agent.state.finished_action:
-                next_vertex, next_action = agent.actions[agent.state.current_action + 1].tuple()
-                if next_vertex is not vertex:
-                    agent.state.is_travelling = (True, vertex, next_vertex)
-                    agent.state.action_time = 0
-                agent.state.current_action += 1
-                agent.state.finished_action = False
+                if agent.state.is_travelling[0]:
+                    # It is travelling to a vertex
+                    _, origin, target = agent.state.is_travelling
+                    edge = self.graph.get_edge(origin, target)
+                    travel_time = edge.time
+                    if agent.state.action_time > travel_time:
+                        agent.state.is_travelling = (False, None, None)
+                        agent.state.just_arrived = True
+                        agent.state.action_time = 0
+                        agent.state.soc -= edge.weight / (self.battery_eff * agent.battery_capacity)
+                else:
+                    # It is doing some action
+                    if agent.state.just_arrived:
+                        vertex.members.append(agent)
+                        agent.state.is_waiting = True
+                        agent.state.just_arrived = False
 
+                    if agent.state.is_waiting:
+                        agent.state.is_waiting = vertex.capacity < len(vertex.members)
+                    else:
+                        time, energy = action.act(agent, vertex)
+                        if agent.state.action_time > time:
+                            agent.state.finished_action = True
+                            agent.state.soc -= energy / (self.battery_eff * agent.battery_capacity)
+
+                # Go to the next action if appropiate
+                if agent.state.finished_action:
+                    # Send the agent to sleep if there are no more actions left
+                    if agent.state.current_action + 1 >= len(agent.actions):
+                        agent.state.is_done = True
+                        agent.state.action_time = 0
+                    else:
+                        print(agent.state.current_action, agent.actions)
+                        next_vertex, _ = agent.actions[agent.state.current_action + 1].tuple()
+                        if next_vertex is not vertex:
+                            agent.state.is_travelling = (True, vertex, next_vertex)
+                            agent.state.action_time = 0
+                        agent.state.current_action += 1
+                        agent.state.finished_action = False
+
+        # Store the data
         try:
             self.data_extractor.append(self)
         except Exception as e:
