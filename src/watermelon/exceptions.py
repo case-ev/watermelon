83d70ccf3c18ac7775a24a3a1e4f102f479efe04
Cristobal Allendes <callendes.molina@gmail.com>
2023-02-14 11:36:29 -0300

    Added an exception for accessing non-existent edges

--- src/watermelon/exceptions.py
+++ src/watermelon/exceptions.py
@@ -8,8 +8,15 @@ Different kinds of exceptions used in the program.
 class ForbiddenActionException(Exception):
     """Exception for a forbidden action"""
 
-    def __init__(self, action, vertex_type) -> None:
+    def __init__(self, action, vertex_type):
         super().__init__(
             f"Action {action.__class__.__name__} not allowed \
 in vertex type {vertex_type.__class__.__name__}"
         )
+
+
+class NonExistentEdgeException(Exception):
+    """Exception for a non existent edge in a graph"""
+
+    def __init__(self, origin, target):
+        super().__init__(f"Edge {origin}->{target} does not exist")
