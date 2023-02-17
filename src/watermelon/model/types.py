dab9e0c2883c855c1b01754dbf0f7a8b2b63a722
Cristobal Allendes <callendes.molina@gmail.com>
2023-02-17 15:25:07 -0300

    Added type annotations to types.py

--- src/watermelon/model/types.py
+++ src/watermelon/model/types.py
@@ -14,17 +14,17 @@ class VertexType(abc.ABC):
 
     @staticmethod
     @abc.abstractmethod
-    def _char():
+    def _char() -> str:
         pass
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return self.__class__.__name__
 
-    def __eq__(self, __o):
+    def __eq__(self, __o) -> bool:
         return self.__class__ is __o.__class__
 
     @classmethod
-    def __str__(cls):
+    def __str__(cls) -> str:
         return cls._char()
 
 
@@ -34,7 +34,7 @@ class EmptyVertexType(VertexType):
     ACTIONS = [actions.NullAction, actions.WaitAction]
 
     @staticmethod
-    def _char():
+    def _char() -> str:
         return "\u03b8"  # theta
 
 
@@ -43,15 +43,15 @@ class EVChargerType(VertexType):
 
     ACTIONS = [actions.ChargeBatteryAction, actions.NullAction, actions.WaitAction]
 
-    def __init__(self, charge_power):
+    def __init__(self, charge_power) -> None:
         self._charge_power = charge_power
 
     @staticmethod
-    def _char():
+    def _char() -> str:
         return "C"
 
     @property
-    def charge_power(self):
+    def charge_power(self) -> float:
         """Power with which the EV charges"""
         return self._charge_power
 
@@ -61,15 +61,15 @@ class MaterialLoadType(VertexType):
 
     ACTIONS = [actions.LoadMaterialAction, actions.NullAction, actions.WaitAction]
 
-    def __init__(self, load_rate):
+    def __init__(self, load_rate) -> None:
         self._load_rate = load_rate
 
     @staticmethod
-    def _char():
+    def _char() -> str:
         return "X"
 
     @property
-    def load_rate(self):
+    def load_rate(self) -> float:
         """Rate in kg/minute at which material is loaded"""
         return self._load_rate
 
@@ -79,14 +79,14 @@ class MaterialDischargeType(VertexType):
 
     ACTIONS = [actions.DischargeMaterialAction, actions.NullAction, actions.WaitAction]
 
-    def __init__(self, discharge_rate):
+    def __init__(self, discharge_rate) -> None:
         self._discharge_rate = discharge_rate
 
     @staticmethod
-    def _char():
+    def _char() -> str:
         return "O"
 
     @property
-    def discharge_rate(self):
+    def discharge_rate(self) -> float:
         """Rate in kg/minute at which material is discharged"""
         return self._discharge_rate
