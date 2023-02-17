4b15b496d838d12d5c08b9ce54fa89ffb3b7ab54
Cristobal Allendes <callendes.molina@gmail.com>
2023-02-17 13:24:02 -0300

    Modelled the uncertainty with classes

---
+++ src/watermelon/model/uncertainty.py
@@ -0,0 +1,40 @@
+"""
+watermelon.model.uncertainty
+----------------------------
+Sources of uncertainty in the model.
+"""
+
+import abc
+
+import numpy as np
+
+
+class UncertaintySource(abc.ABC):
+    """Abstract source of uncertainty"""
+
+    @property
+    @abc.abstractmethod
+    def last(self):
+        """Gets the last sample that was taken from the distribution"""
+
+    @abc.abstractmethod
+    def sample(self):
+        """Get a sample of the distribution"""
+
+
+class GaussianUncertainty(UncertaintySource):
+    """Model for gaussian uncertainty"""
+
+    def __init__(self, mean=0, std=1):
+        self.mean = mean
+        self.std = std
+        self._last_sample = None
+
+    @property
+    def last(self):
+        return self._last_sample
+
+    def sample(self):
+        value = np.random.default_rng().normal(self.mean, self.std)
+        self._last_sample = value
+        return value
