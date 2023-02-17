d9c8e4efa3e022365a07089ae755449dc8ea4537
Cristobal Allendes <callendes.molina@gmail.com>
2023-02-17 15:24:09 -0300

    Added type annotations

--- src/watermelon/model/uncertainty.py
+++ src/watermelon/model/uncertainty.py
@@ -14,41 +14,41 @@ class UncertaintySource(abc.ABC):
 
     @property
     @abc.abstractmethod
-    def last(self):
+    def last(self) -> float:
         """Gets the last sample that was taken from the distribution"""
 
     @abc.abstractmethod
-    def sample(self):
+    def sample(self) -> float:
         """Get a sample of the distribution"""
 
 
 class NoUncertainty(UncertaintySource):
     """Model for a deterministic output"""
 
-    def __init__(self):
+    def __init__(self) -> None:
         self._last_sample = 0
 
     @property
-    def last(self):
+    def last(self) -> float:
         return self._last_sample
 
-    def sample(self):
+    def sample(self) -> float:
         return 0
 
 
 class GaussianUncertainty(UncertaintySource):
     """Model for gaussian uncertainty"""
 
-    def __init__(self, mean=0, std=0.001):
+    def __init__(self, mean: float = 0, std: float = 0.001) -> None:
         self.mean = mean
         self.std = std
         self._last_sample = None
 
     @property
-    def last(self):
+    def last(self) -> float:
         return self._last_sample
 
-    def sample(self):
+    def sample(self) -> float:
         value = np.random.default_rng().normal(self.mean, self.std)
         self._last_sample = value
         return value
