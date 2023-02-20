6529d5fcba92e67dd7e924b340a3cc8f27db1e87
Cristobal Allendes <callendes.molina@gmail.com>
2023-02-20 17:57:42 -0300

    Manually added all file names to specify the desired order

--- sys/doc.sh
+++ sys/doc.sh
@@ -8,7 +8,13 @@ cd docs
 
 BUILD_DIR="../build/docs"
 target="html"
-files=""
+FILES=(
+    examples.md
+    graph.md
+    vertices.md
+    agents.md
+    soc.md
+)
 
 while getopts "hpT:" arg
 do
@@ -33,12 +39,11 @@ for file in *; do
         title=${title#\# }
         cat "$file" | tail -n +2 > "${BUILD_DIR}/temp/${filename}.md"
         pandoc -f markdown -t $target "${BUILD_DIR}/temp/${filename}.md" -o "${BUILD_DIR}/${filename}.${target}" --lua-filter lua/link_files.lua --metadata title="$title" --template pandoc-toc-sidebar/toc-sidebar.html --toc -B template/nav
-        files+=" ${filename}.md"
     fi
 done
 
 echo -e "Compiling \x1b[32;20mall files\x1b[0m"
-pandoc -f markdown -t $target $files -o "${BUILD_DIR}/all.${target}" --lua-filter lua/link_files.lua --embed-resources --standalone --metadata title="Watermelon - docs" --template pandoc-toc-sidebar/toc-sidebar.html --toc -B template/nav
+pandoc -f markdown -t $target ${FILES[@]} -o "${BUILD_DIR}/all.${target}" --lua-filter lua/link_files.lua --embed-resources --standalone --metadata title="Watermelon - docs" --template pandoc-toc-sidebar/toc-sidebar.html --toc -B template/nav
 
 echo -e "Cleaning up"
 rm -rf "${BUILD_DIR}/temp/"
