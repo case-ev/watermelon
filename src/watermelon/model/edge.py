abe9af4d0b6b9e00904d91b719cffe0984d97057
Cristobal Allendes <callendes.molina@gmail.com>
2023-02-20 11:41:10 -0300

    Added type annotations to edge.py

--- src/watermelon/model/edge.py
+++ src/watermelon/model/edge.py
@@ -4,17 +4,19 @@ watermelon.model.edge
 Definition of edges in the graph, which connect two vertices.
 """
 
+from watermelon.model.vertex import Vertex
+
 
 class Edge:
     """Edge connecting two vertices"""
 
-    def __init__(self, origin, target, weight=None, time=None):
+    def __init__(self, origin: Vertex, target: Vertex, weight: float = None, time: float = None) -> None:
         self.origin = origin
         self.target = target
         self.weight = weight
         self.time = time
 
-    def __eq__(self, __o):
+    def __eq__(self, __o: object) -> bool:
         if not isinstance(__o, self.__class__):
             return False
 
@@ -25,9 +27,9 @@ class Edge:
             and self.time == __o.time
         )
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"Edge(origin={repr(self.origin)}, target={repr(self.target)}, \
 weight={repr(self.weight)}, time={repr(self.time)})"
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"({str(self.origin)}->{str(self.target)}; w={str(self.weight)}, t={str(self.time)})"
