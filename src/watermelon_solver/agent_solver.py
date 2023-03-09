5dd61aec77021fc4e169c92e924183aa8cd78bb2
Cristobal Allendes <callendes.molina@gmail.com>
2023-03-09 16:41:15 -0300

    Added the skeletons for each solver

--- src/watermelon_solver/agent_solver.py
+++ src/watermelon_solver/agent_solver.py
@@ -4,3 +4,17 @@ watermelon_solver.agent_solver
 Class that abstracts away the process of finding the optimal actions for a
 group of agents.
 """
+
+from typing import Dict, List
+
+import watermelon as wm
+
+
+class AgentSolver:
+    """Class that finds the best actions that some agents can take"""
+
+    def __call__(self, graph: wm.Graph) -> Dict[wm.Agent, List[wm.VertexAction]]:
+        return self.solve(graph)
+
+    def solve(self, graph: wm.Graph) -> Dict[wm.Agent, List[wm.VertexAction]]:
+        """Find the best actions"""
--- src/watermelon_solver/charger_solver.py
+++ src/watermelon_solver/charger_solver.py
@@ -4,3 +4,17 @@ watermelon_solver.charger_solver
 Class that abstracts away the process of finding the optimal locations
 of chargers in a given graph.
 """
+
+from typing import List
+
+import watermelon as wm
+
+
+class ChargerSolver:
+    """Class that finds the best locations for chargers in a graph"""
+
+    def __call__(self, graph: wm.Graph) -> List[bool]:
+        return self.solve(graph)
+
+    def solve(self, graph: wm.Graph) -> List[bool]:
+        """Find the best locations"""
