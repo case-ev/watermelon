3f1a91730894ce3e0d1ff7dfecd4291369714201
Cristobal Allendes <callendes.molina@gmail.com>
2023-03-09 15:47:07 -0300

    Made the basic file structure

---
+++ src/watermelon/solver/code/__init__.py
@@ -0,0 +1,5 @@
+"""
+watermelon.solver.code
+----------------------
+Module in charge of encoding the information of a graph and agents.
+"""
---
+++ src/watermelon/solver/fitness/__init__.py
@@ -0,0 +1,5 @@
+"""
+watermelon.solver.fitness
+-------------------------
+Module of watermelon.solver that handles the fitness functions.
+"""
