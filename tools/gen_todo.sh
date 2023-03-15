acd4e1dbff49ab718cf5dd341036805eb31a5511
Cristobal Allendes <callendes.molina@gmail.com>
2023-03-14 21:24:26 -0300

    Added a gen_todo.sh script to generate a TODO.md file with all TODOs in the source code

---
+++ tools/gen_todo.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Run tools from the root directory of the project
+
+echo -e "# TODO\nThis file contains a list of the TODO tasks found within the source code of *watermelon*.\n\n" > TODO.md
+
+patterns=$(grep -Er "[\/#]* TODO: ([\w\s]*)" ./*)
+
+while read n; do
+    filename=`echo $n | cut -d : -f 1`
+    message=`echo $n | cut -d : -f 3`
+    if [[ "$message" != ' ([ws]*)" ./*)' ]]; then
+        echo -e "`cat TODO.md`\n- **(\`${filename}\`)**${message}" > TODO.md
+    fi
+done <<< "$patterns"
