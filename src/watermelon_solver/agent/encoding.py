2b5aef5a9ba9637fbf5c613636fe9c25dffe0c4a
Cristobal Allendes <callendes.molina@gmail.com>
2023-03-15 09:36:55 -0300

    Added TODOs to the ActionDecoder

--- src/watermelon_solver/agent/encoding.py
+++ src/watermelon_solver/agent/encoding.py
@@ -5,7 +5,7 @@ Decoder that turns a binary code into a list of actions that a given
 agent takes.
 """
 
-from typing import Iterable
+from typing import Iterable, Tuple
 
 import watermelon as wm
 
@@ -13,8 +13,10 @@ import watermelon as wm
 class ActionDecoder:
     """Decoder for the actions of an agent"""
 
-    def __call__(self, word: Iterable[bool]) -> Iterable[wm.VertexAction]:
+    def __call__(self, word: Iterable[bool]) -> Iterable[Tuple[wm.Vertex, wm.VertexAction]]:
         return self.decode(word)
 
-    def decode(self, word: Iterable[bool]) -> Iterable[wm.VertexAction]:
+    def decode(self, word: Iterable[bool]) -> Iterable[Tuple[wm.Vertex, wm.VertexAction]]:
         """Decode the given word into a list of actions"""
+        # TODO: Figure out an encoding scheme for the actions
+        # TODO: Implement decoding for the actions
